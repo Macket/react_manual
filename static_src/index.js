0ec86a5d334d87098f5729f77f540b125a89f91a
Makeev Ivan <makeev@phystech.edu>
2019-08-06 13:09:54 +0300

    Урок 2. Жизненный цикл React-компонента

--- static_src/index.js
+++ static_src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const content = 'Кажется, мы подключили React';
 
-const element = (content) => <h1 className="element">{content}</h1>;
+let messages = ['Привет', 'Как дела?'];
+
+const MessageComponent = (props) => <div>{props.text}</div>;
+
+const MessageField = (props) => {
+    return props.messages.map(message => <MessageComponent text={ message } />);
+};
 
 ReactDOM.render(
-    element(content),
+    <MessageField messages={ messages } />,
     document.getElementById('root'),
 );
+
+
+// const content = 'Кажется, мы подключили React';
+//
+// const Component = (props) => <h1 className="element">{props.content}</h1>;
+//
+// ReactDOM.render(
+//     <Component content={ content } />,
+//     document.getElementById('root'),
+// );
