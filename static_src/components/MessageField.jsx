72f3031fe26e87f471344d17d7fcd14649f3bcfe
Makeev Ivan <makeev@phystech.edu>
2019-08-08 13:46:06 +0300

    Урок 3. Продолжаем погружение в React и подключаем UI-библиотеку

--- static_src/components/MessageField.jsx
+++ static_src/components/MessageField.jsx
@@ -1,35 +1,67 @@
 import React from 'react';
+import { TextField, FloatingActionButton } from 'material-ui';
+import SendIcon from 'material-ui/svg-icons/content/send';
 import Message from './Message';
+import '../styles/styles.css';
 
 export default class MessageField extends React.Component {
     state = {
-        messages: ["Привет!", "Как дела?"]
+        messages: [{ text: "Привет!", sender: 'bot' }, { text: "Как дела?", sender: 'bot' }],
+        input: '',
     };
-
-    handleClick = () => {
-        this.setState({ messages: [ ...this.state.messages, 'Нормально' ] });
-    };
-
-    componentDidMount() {
-        console.log(__dirname)
-    }
 
     componentDidUpdate() {
-        if (this.state.messages.length % 2 === 1) {
+        if (this.state.messages[this.state.messages.length - 1].sender === 'me') {
             setTimeout(() =>
-            this.setState(
-                { messages: [ ...this.state.messages, 'Не приставай ко мне, я робот!' ] }),
-            1000);
+                    this.setState({
+                        messages: [ ...this.state.messages, {text: 'Не приставай ко мне, я робот!', sender: 'bot'} ] }),
+                1000);
         }
     }
 
-    render() {
-        const messageElements = this.state.messages.map((text, index) => (
-            <Message key={ index } text={ text } />));
+    handleClick = (message) => {
+        this.sendMessage(message)
+    };
 
-        return <div>
-            { messageElements }
-            <button onClick={ this.handleClick }>Отправить сообщение</button>
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    handleKeyUp = (event, message) => {
+        if (event.keyCode === 13) { // Enter
+            this.sendMessage(message)
+        }
+    };
+
+    sendMessage = (message) => {
+        this.setState({
+            messages: [ ...this.state.messages, {text: message, sender: 'me'} ],
+            input: '',
+        });
+    };
+
+    render() {
+        const messageElements = this.state.messages.map((message, index) => (
+            <Message key={ index } text={ message.text } sender={ message.sender } />));
+
+        return <div className="layout">
+            <div className="message-field">
+                { messageElements }
+            </div>
+            <div style={ { width: '100%', display: 'flex' } }>
+                <TextField
+                    name="input"
+                    fullWidth={ true }
+                    hintText="Введите сообщение"
+                    style={ { fontSize: '22px' } }
+                    onChange={ this.handleChange }
+                    value={ this.state.input }
+                    onKeyUp={ (event) => this.handleKeyUp(event, this.state.input) }
+                />
+                <FloatingActionButton onClick={ () => this.handleClick(this.state.input) }>
+                    <SendIcon />
+                </FloatingActionButton>
+            </div>
         </div>
     }
 }
--- static_src/index.js
+++ static_src/index.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
-let messages = ['Привет', 'Как дела?'];
-
-const MessageComponent = (props) => <div>{props.text}</div>;
-
-const MessageField = (props) => {
-    return props.messages.map(message => <MessageComponent text={ message } />);
-};
+import MessageField from './components/MessageField'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 ReactDOM.render(
-    <MessageField messages={ messages } />,
+    <MuiThemeProvider>
+        <MessageField />
+    </MuiThemeProvider>,
     document.getElementById('root'),
 );
-
-
-// const content = 'Кажется, мы подключили React';
-//
-// const Component = (props) => <h1 className="element">{props.content}</h1>;
-//
-// ReactDOM.render(
-//     <Component content={ content } />,
-//     document.getElementById('root'),
-// );
